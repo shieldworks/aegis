82cbd475852bccff2bb463142ecea9cd4de6b316
Volkan Özçelik <me@volkan.io>
2022-12-17 09:37:02 -0800

    demo app: initial

--- demo/main.go
+++ demo/main.go
@@ -1,15 +1,31 @@
 /*
  * .-'_.---._'-.
- * ||####|(__)|| Protect your secrets, protect your business.
- *   \\()|##//     Secure your sensitive data with Aegis.
- *    \\ |#//                <aegis.z2h.dev>
+ * ||####|(__)||   Protect your secrets, protect your business.
+ *   \\()|##//       Secure your sensitive data with Aegis.
+ *    \\ |#//                  <aegis.z2h.dev>
  *     .\_/.
  */
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+)
 
 func main() {
 	fmt.Println("Hello world, hello stars, hello universe!")
+
+	const secretFilePath = "/opt/aegis/secrets.json"
+
+	for {
+		dat, err := os.ReadFile(secretFilePath)
+		if err != nil {
+			panic("Failed to read secrets file.")
+		}
+		fmt.Print(string(dat))
+
+		time.Sleep(5 * time.Second)
+	}
 }
