33be8bb6a9c072e8da04bc2e34d58e4945aee8d2
Volkan Özçelik <me@volkan.io>
2023-01-03 11:08:39 -0800

    workload is able to get svid

--- sidecar/internal/sentry/fetch.go
+++ sidecar/internal/sentry/fetch.go
@@ -10,8 +10,11 @@ package sentry
 
 import (
 	"bufio"
+	"context"
 	"log"
 	"os"
+
+	"github.com/spiffe/go-spiffe/v2/workloadapi"
 )
 
 func saveData(data string) {
@@ -48,6 +51,17 @@ func fetchSecrets() {
 	// 2. Get SafeSpiffeId from environment.
 	// 3. Send a GET request to Safe (safe can know who you are from your spiffeid)
 	// 4. parse and safe the returned data.
+
+	const socketPath = "unix:///spire-agent-socket/agent.sock"
+
+	ctx := context.Background()
+
+	source, err := workloadapi.NewX509Source(ctx, workloadapi.WithClientOptions(workloadapi.WithAddr(socketPath)))
+	if err != nil {
+		log.Println("Unable to create X509 source")
+	} else {
+		log.Println("Everything is awesome!", source)
+	}
 
 	log.Println("WILL FETCH SECRETS!!!")
 
