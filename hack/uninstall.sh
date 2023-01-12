8858ec4fa7cb6c854311c8939d1bda12efdfbfd8
Volkan Özçelik <me@volkan.io>
2023-01-11 20:59:22 -0800

    minor

--- hack/uninstall.sh
+++ hack/uninstall.sh
@@ -10,12 +10,10 @@
 
 if kubectl get ns | grep aegis-system; then
   kubectl delete ns spire-system
-	kubectl delete ns aegis-system
-	kubectl delete ClusterSPIFFEID aegis-workload-demo
-	kubectl delete ClusterSPIFFEID aegis-sentinel
-	kubectl delete ClusterSPIFFEID aegis-safe
-
-	echo "Everything is awesome!"
+  kubectl delete ns aegis-system
+  kubectl delete ClusterSPIFFEID aegis-workload-demo
+  kubectl delete ClusterSPIFFEID aegis-sentinel
+  kubectl delete ClusterSPIFFEID aegis-safe
 else
   echo "Nothing to clean."
 fi
