d1bb2a7ee3c68ff70ec452b080aeea28180c94c9
Volkan Özçelik <me@volkan.io>
2023-01-26 21:49:13 -0800

    poor man’s smoke test

--- hack/test/sed.sh
+++ hack/test/sed.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+#
+# .-'_.---._'-.
+# ||####|(__)||   Protect your secrets, protect your business.
+#   \\()|##//       Secure your sensitive data with Aegis.
+#    \\ |#//                  <aegis.z2h.dev>
+#     .\_/.
+#
+
 OLD=aegis-sentinel-58f6478b79-6g242
 NEW=aegis-sentinel-58f6478b79-6g242
 
