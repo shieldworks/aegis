8b1f6d8e1dbbd08f1693d3be945a8f15f0b3c9c1
Volkan Özçelik <me@volkan.io>
2023-02-04 06:56:13 -0800

    pull change

--- hack/pull.sh
+++ hack/pull.sh
@@ -15,22 +15,31 @@ echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   echo "aegis-spire"
+  mkdir -p aegis-spire
 	cd aegis-spire || exit; git stash; git checkout main; git pull;
 	echo "aegis-core"
+	mkdir -p ../aegis-core
 	cd ../aegis-core || exit; git stash; git checkout main; git pull;
 	echo "aegis-sdk-go"
+	mkdir -p ../aegis-sdk-go
 	cd ../aegis-sdk-go || exit; git stash; git checkout main; git pull;
 	echo "aegis-sentinel"
+	mkdir -p ../aegis-sentinel
 	cd ../aegis-sentinel || exit; git stash; git checkout main; git pull;
 	echo "aegis-sidecar"
+	mkdir -p ../aegis-sidecar
 	cd ../aegis-sidecar || exit; git stash; git checkout main; git pull;
 	echo "aegis-safe"
+	mkdir -p ../aegis-safe
 	cd ../aegis-safe || exit; git stash; git checkout main; git pull;
 	echo "aegis-web"
+	mkdir -p ../aegis-web
 	cd ../aegis-web || exit; git stash; git checkout main; git pull;
 	echo "aegis-workload-demo-using-sidecar"
+	mkdir -p ../aegis-workload-demo-using-sidecar
 	cd ../aegis-workload-demo-using-sidecar || exit; git stash; git checkout main; git pull;
 	echo "aegis-workload-demo-using-sdk"
+	mkdir -p ../aegis-workload-demo-using-sdk
 	cd ../aegis-workload-demo-using-sdk || exit; git stash; git checkout main; git pull;
 	echo "Everything is awesome!"
 fi
