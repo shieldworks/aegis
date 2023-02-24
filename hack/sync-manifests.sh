be245fed5fed68a4dff70f9a1d85f4b1b1a2227b
Volkan Özçelik <me@volkan.io>
2023-02-23 20:08:32 -0800

    added init container to pull script

--- hack/sync-manifests.sh
+++ hack/sync-manifests.sh
@@ -14,6 +14,7 @@ mkdir -p ./install/k8s/demo-workload
 
 cp -rv ../aegis-workload-demo-using-sdk/k8s ./install/k8s/demo-workload/using-sdk
 cp -rv ../aegis-workload-demo-using-sidecar/k8s ./install/k8s/demo-workload/using-sidecar
+cp -rv ../aegis-workload-demo-using-init-container/k8s ./install/k8s/demo-workload/using-init-container
 cp -rv ../aegis-safe/k8s ./install/k8s/safe
 cp -rv ../aegis-sentinel/k8s ./install/k8s/sentinel
 cp -rv ../aegis-spire/k8s ./install/k8s/spire
