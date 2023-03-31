6daa4d3f3f2e1f2f84cdec39eed7a3690e5f9668
Volkan Özçelik <me@volkan.io>
2023-03-30 22:57:40 -0700

    fixed script; it just needs to tag and push; build is done elsewhere.

--- hack/push.sh
+++ hack/push.sh
@@ -13,7 +13,6 @@ PACKAGE="$1"
 VERSION="$2"
 REPO="$3"
 
-docker build . -t "${PACKAGE}":"${VERSION}"
 docker tag "${PACKAGE}":"${VERSION}" "${REPO}":"${VERSION}"
 docker push "${REPO}":"${VERSION}"
 
